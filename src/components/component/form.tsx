96b94def1ce6c7e7d75381d6f2b78c12ece35158
mh0070554 <mh0070554@gmail.com>
2024-04-12 16:49:25 +0100

    work

--- src/components/component/form.tsx
+++ src/components/component/form.tsx
@@ -15,46 +15,77 @@ import * as fbq from "../../lib/fpixel";
 const rubik = Noto_Kufi_Arabic({ subsets: ['arabic'],
  weight:['500','600','700'],
  })
-export function Form() {
-  const [formData, setFormData] = useState<{ [key: string]: string | string[] }>({
-    facebook: '',
-    group: '',
-    email: '',
-    website: '',
-    specialty: [],
-  });
+ export function Form() {
+const [formData, setFormData] = useState<{
+  facebook: string;
+  group: string;
+  email: string;
+  website: string;
+  specialty: string[];
+}>({
+  facebook: '',
+  group: '',
+  email: '',
+  website: '',
+  specialty: [],
+});
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value, type, checked } = e.target;
-    if (type === 'checkbox') {
-      setFormData(prevData => ({
-        ...prevData,
-        [name]: checked
-          ? [...(prevData[name] as string[]), value]
-          : (prevData[name] as string[]).filter(item => item !== value),
-      }));
-    } else {
-      setFormData(prevData => ({ ...prevData, [name]: value }));
-    }
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData(prevData => ({ ...prevData, [name]: value }));
   };
+
+  const initialCheckboxState = {
+    'تصميم UI/UX': false,
+    'تصميم الجرافيك': false,
+    'تصميم الويب': false,
+    'تصميم المنتج و التفليف': false,
+    'الرسم التوضيحي (الإليستريشن)': false,
+    'رسوم متحركة (موشن جرافيكس)': false,
+    'مونتاج الفيديو': false,
+    'التصوير الفوتوغرافي': false,
+  };
+  
+  // Use the initial state in the useState hook
+  const [checkboxState, setCheckboxState] = useState(initialCheckboxState);
+  
+  // Update the handleCheckboxChange function
+  const handleCheckboxChange = (name: string, checked: boolean) => {
+    setCheckboxState(prevState => ({ ...prevState, [name]: checked }));
+  };
+  
+  
   
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const eventID: string = crypto.randomUUID();
-  
-    const eventTime = Math.floor(Date.now() / 1000); // new Date();: 
-    const userIp = await getUserIP();
-    const message = `
+  e.preventDefault();
+
+const selectedSpecialties = Object.keys(checkboxState).filter(
+  (specialty: string) => checkboxState[specialty as keyof typeof checkboxState]
+);
+
+  // Merge selectedSpecialties into the specialty field in formData
+  const updatedFormData = {
+    ...formData,
+    specialty: Array.isArray(formData.specialty)
+      ? [...(formData.specialty as string[]), ...selectedSpecialties]
+      : selectedSpecialties,
+  };
+
+  // Now use updatedFormData instead of formData
+  const eventID: string = crypto.randomUUID();
+  const eventTime = Math.floor(Date.now() / 1000);
+  const userIp = await getUserIP();
+  const message = `
     profile: ${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}
     IP: ${userIp}
     eventTime: ${eventTime}
     eventID: ${eventID}
-اسمك على الفايسبوك: "${formData.facebook}" \n
-اسم المجموعة: "${formData.group}" \n
-البريد الإلكتروني: "${formData.email}" \n
-portfolio / تصاميمك: "${formData.website}"\n
-تخصص التصميم: ${(formData.specialty as string[]).join(', ')}
+    اسمك على الفايسبوك: "${formData.facebook}" \n
+    اسم المجموعة: "${formData.group}" \n
+    البريد الإلكتروني: "${formData.email}" \n
+    portfolio / تصاميمك: "${formData.website}"\n
+    تخصص التصميم: ${(updatedFormData.specialty as string[]).join(', ')}
   `;
   const email: string = Array.isArray(formData.email) ? formData.email[0] : formData.email;
   const messageCompleteRegistration = `{
@@ -73,7 +104,7 @@ portfolio / تصاميمك: "${formData.website}"\n
             }
         }
     ], 
-}`;
+  }`;
     console.log(formData)
     const additionalData = {};
     await sendToTelegram(message);
@@ -102,7 +133,7 @@ portfolio / تصاميمك: "${formData.website}"\n
         <div className="space-y-2">
           <Label htmlFor="facebook" className="text-lg">اسمك على الفايسبوك</Label>
           <span className="text-red-500">*</span>
-          <Input id="facebook" name="facebook" placeholder="أدخل رابط حسابك على الفيسبوك" required type="text" value={formData.facebook} onChange={handleChange} />
+          <Input id="facebook" name="facebook" placeholder="أدخل رابط حسابك على الفيسبوك" required type="text" value={formData.facebook} onChange={handleInputChange} />
           <p className="text-base text-gray-500 dark:text-gray-400">
             ( نحن بحاجة إلى هذا حتى نتمكن من الاتصال بك عندما تفوز.)
           </p>
@@ -112,91 +143,145 @@ portfolio / تصاميمك: "${formData.website}"\n
             اسم المجموعة
             <span className="text-red-500">*</span>
           </Label>
-          <Input id="group" name="group" placeholder="أدخل اسم مجموعتك" required type="text" value={formData.group} onChange={handleChange} />
+          <Input id="group" name="group" placeholder="أدخل اسم مجموعتك" required type="text" value={formData.group} onChange={handleInputChange} />
           <p className="text-base text-gray-500 dark:text-gray-400">اسم المجموعة التي تشارك منها!.</p>
         </div>
         <div className="space-y-4">
           <p className="text-base text-black dark:text-gray-100 font-bold">
             فقط اذا اردت المشاركة في السحب على الجائزة الأولى املأ التالي.
           </p>
+          </div>
           <div className="space-y-2">
             <Label htmlFor="email" className="text-lg">البريد الإلكتروني (اختياري)</Label>
-            <Input id="email" name="email" placeholder="أدخل بريدك الإلكتروني" type="email" value={formData.email} onChange={handleChange} />
+            <Input id="email" name="email" placeholder="أدخل بريدك الإلكتروني" type="email" value={formData.email} onChange={handleInputChange} />
           </div>
           <div className="space-y-2">
             <Label htmlFor="website" className="text-lg">portfolio / تصاميمك (اختياري)</Label>
-            <Input id="website" name="website" placeholder="أدخل رابط صفحة اعمالك" type="url" value={formData.website} onChange={handleChange} />
+            <Input id="website" name="website" placeholder="أدخل رابط صفحة اعمالك" type="url" value={formData.website} onChange={handleInputChange} />
           </div>
           <div className="space-y-2">
             <Label htmlFor="specialty" className="text-lg">تخصص التصميم (اختياري)</Label>
             <div className="grid grid-cols-2 gap-2 space-y-2" dir="rtl">
             <div className="flex items-center space-x-2">
-              <Checkbox
-                id="uiux"
-                name="specialty"
-                value="تصميم UI/UX"
-                checked={formData.specialty.includes('تصميم UI/UX')}
-                onChange={handleChange}
-              />
-              <Label
-                className="ml-2 text-base font-medium text-gray-950 dark:text-gray-400"
-                htmlFor="uiux"
-              >
-                تصميم UI/UX
-              </Label>
-            </div>
-              <div className="flex items-center space-x-2">
-                <Checkbox
-                id="graphic"
-                name="specialty"
-                value="تصميم الجرافيك"
-                checked={formData.specialty.includes('تصميم الجرافيك')}
-                onChange={handleChange}
-              />
-              <Label
-                className="ml-2 text-base font-medium text-gray-900 dark:text-gray-400"
-                htmlFor="graphic"
-              >
-                تصميم الجرافيك
-              </Label>
-              </div>
-              <div className="flex items-center space-x-2">
-                <Checkbox className="rounded text-blue-500" id="web" name="specialty" value="تصميم الويب" checked={formData.specialty.includes( 'تصميم الويب')} onChange={handleChange} />
-                <Label className="ml-2 text-base font-medium text-gray-900 dark:text-gray-400" htmlFor="web">
-                  تصميم الويب
-                </Label>
-              </div>
-              <div className="flex items-center space-x-2">
-                <Checkbox className="rounded text-blue-500" id="product" name="specialty" value="تصميم المنتج و التفليف" checked={formData.specialty.includes('تصميم المنتج و التفليف')} onChange={handleChange} />
-                <Label className="ml-2 text-base font-medium text-gray-900 dark:text-gray-400" htmlFor="product">
-                  تصميم المنتج و التفليف
-                </Label>
-              </div>
-              <div className="flex items-center space-x-2">
-                <Checkbox className="rounded text-blue-500" id="illustration" name="specialty" value="الرسم التوضيحي (الإليستريشن)" checked={formData.specialty.includes('الرسم التوضيحي (الإليستريشن)')} onChange={handleChange} />
-                <Label className="ml-2 text-base font-medium text-gray-900 dark:text-gray-400" htmlFor="illustration">
-                  الرسم التوضيحي (الإليستريشن)
-                </Label>
-              </div>
-              <div className="flex items-center space-x-2">
-                <Checkbox className="rounded text-blue-500" id="motiongraphics" name="specialty" value="رسوم متحركة (موشن جرافيكس)" checked={formData.specialty.includes('رسوم متحركة (موشن جرافيكس)')} onChange={handleChange} />
-                <Label className="ml-2 text-base font-medium text-gray-900 dark:text-gray-400" htmlFor="motiongraphics">
-                  رسوم متحركة (موشن جرافيكس)
-                </Label>
-              </div>
-              <div className="flex items-center space-x-2">
-                <Checkbox className="rounded text-blue-500" id="videoediting" name="specialty" value="مونتاج الفيديو" checked={formData.specialty.includes('مونتاج الفيديو')} onChange={handleChange} />
-                <Label className="ml-2 text-base font-medium text-gray-900 dark:text-gray-400" htmlFor="videoediting">
-                  مونتاج الفيديو
-                </Label>
-              </div>
-              <div className="flex items-center space-x-2">
-                <Checkbox className="rounded text-blue-500" id="photography" name="specialty" value="التصوير الفوتوغرافي" checked={formData.specialty.includes('التصوير الفوتوغرافي')} onChange={handleChange} />
-                <Label className="ml-2 text-base font-medium text-gray-900 dark:text-gray-400" htmlFor="photography">
-                  التصوير الفوتوغرافي
-                </Label>
-              </div>
-            </div>
+            <Checkbox
+            id="uiux"
+            name="specialty"
+            value="تصميم UI/UX"
+            checked={checkboxState['تصميم UI/UX']}
+            onCheckedChange={(checked) => handleCheckboxChange('تصميم UI/UX', checked)}
+          />
+          <Label
+            className="ml-2 text-base font-medium text-gray-950 dark:text-gray-400"
+            htmlFor="uiux"
+          >
+            تصميم UI/UX
+          </Label>
+        </div>
+        <div className="flex items-center space-x-2">
+  <Checkbox
+    id="graphic"
+    name="specialty"
+    value="تصميم الجرافيك"
+    checked={checkboxState['تصميم الجرافيك']}
+    onCheckedChange={(checked) => handleCheckboxChange('تصميم الجرافيك', checked)}
+  />
+  <Label
+    className="ml-2 text-base font-medium text-gray-900 dark:text-gray-400"
+    htmlFor="graphic"
+  >
+    تصميم الجرافيك
+  </Label>
+</div>
+<div className="flex items-center space-x-2">
+  <Checkbox
+    id="web"
+    name="specialty"
+    value="تصميم الويب"
+    checked={checkboxState['تصميم الويب']}
+    onCheckedChange={(checked) => handleCheckboxChange('تصميم الويب', checked)}
+  />
+  <Label
+    className="ml-2 text-base font-medium text-gray-900 dark:text-gray-400"
+    htmlFor="web"
+  >
+    تصميم الويب
+  </Label>
+</div>
+<div className="flex items-center space-x-2">
+  <Checkbox
+    id="product"
+    name="specialty"
+    value="تصميم المنتج و التفليف"
+    checked={checkboxState['تصميم المنتج و التفليف']}
+    onCheckedChange={(checked) => handleCheckboxChange('تصميم المنتج و التفليف', checked)}
+  />
+  <Label
+    className="ml-2 text-base font-medium text-gray-900 dark:text-gray-400"
+    htmlFor="product"
+  >
+    تصميم المنتج و التفليف
+  </Label>
+</div>
+<div className="flex items-center space-x-2">
+  <Checkbox
+    id="illustration"
+    name="specialty"
+    value="الرسم التوضيحي (الإليستريشن)"
+    checked={checkboxState['الرسم التوضيحي (الإليستريشن)']}
+    onCheckedChange={(checked) => handleCheckboxChange('الرسم التوضيحي (الإليستريشن)', checked)}
+  />
+  <Label
+    className="ml-2 text-base font-medium text-gray-900 dark:text-gray-400"
+    htmlFor="illustration"
+  >
+    الرسم التوضيحي (الإليستريشن)
+  </Label>
+</div>
+<div className="flex items-center space-x-2">
+  <Checkbox
+    id="motiongraphics"
+    name="specialty"
+    value="رسوم متحركة (موشن جرافيكس)"
+    checked={checkboxState['رسوم متحركة (موشن جرافيكس)']}
+    onCheckedChange={(checked) => handleCheckboxChange('رسوم متحركة (موشن جرافيكس)', checked)}
+  />
+  <Label
+    className="ml-2 text-base font-medium text-gray-900 dark:text-gray-400"
+    htmlFor="motiongraphics"
+  >
+    رسوم متحركة (موشن جرافيكس)
+  </Label>
+</div>
+<div className="flex items-center space-x-2">
+  <Checkbox
+    id="videoediting"
+    name="specialty"
+    value="مونتاج الفيديو"
+    checked={checkboxState['مونتاج الفيديو']}
+    onCheckedChange={(checked) => handleCheckboxChange('مونتاج الفيديو', checked)}
+  />
+  <Label
+    className="ml-2 text-base font-medium text-gray-900 dark:text-gray-400"
+    htmlFor="videoediting"
+  >
+    مونتاج الفيديو
+  </Label>
+</div>
+<div className="flex items-center space-x-2">
+  <Checkbox
+    id="photography"
+    name="specialty"
+    value="التصوير الفوتوغرافي"
+    checked={checkboxState['التصوير الفوتوغرافي']}
+    onCheckedChange={(checked) => handleCheckboxChange('التصوير الفوتوغرافي', checked)}
+  />
+  <Label
+    className="ml-2 text-base font-medium text-gray-900 dark:text-gray-400"
+    htmlFor="photography"
+  >
+    التصوير الفوتوغرافي
+  </Label>
+</div>
           </div>
           <Button className="w-full" type="submit">
             أدخل للفوز والترقية!
